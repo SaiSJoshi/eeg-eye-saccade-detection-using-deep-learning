8a7729fdb2060b145fa6227d4e8eba740430f67c
Yidan-Ding <115201635+Yidan-Ding@users.noreply.github.com>
2022-11-03 17:35:35 -0400

    Add files via upload
    Dataset class with split function added, can work now

--- Dataset.py
+++ Dataset.py
@@ -1,55 +1,66 @@
+import numpy as np
+import torch
+import math
 
-data=np.load('data\LR_Task.npz')
+
+def split(ids, train, val, test):
+    assert (train+val+test == 1)
+    IDs = np.unique(ids)
+    num_ids = len(IDs)
+
+    # priority given to the test/val sets
+    test_split = math.ceil(test * num_ids)
+    val_split = math.ceil(val * num_ids)
+    train_split = num_ids - val_split - test_split
+
+    train = np.isin(ids, IDs[:train_split])
+    val = np.isin(ids, IDs[train_split:train_split+val_split])
+    test = np.isin(ids, IDs[train_split+val_split:])
+
+    return train, val, test
+
 
 class Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, data, partition): # Feel free to add more arguments
-        
-        if (partition=="train"):
-            data=np.random(data,70) # change this 
-        elif (partition=="val"):
-            data=np.random(data,70)
+    def __init__(self, data_path, train_ratio, val_ratio, test_ratio, partition):
+
+        whole_data = np.load(data_path)
+        whole_eeg = whole_data['EEG']
+        whole_label = whole_data['labels']
+        ids = whole_label[:, 0]
+        whole_label = whole_label[:, 1:]
+
+        # Split the data
+        train_idx, val_idx, test_idx = split(
+            ids, train_ratio, val_ratio, test_ratio)
+
+        if (partition == "train"):
+            EEG = whole_eeg[train_idx]
+            label = whole_label[train_idx]
+        elif (partition == "val"):
+            EEG = whole_eeg[val_idx]
+            label = whole_label[val_idx]
         else:
-            data=np.random(data,70)
+            EEG = whole_eeg[test_idx]
+            label = whole_label[test_idx]
 
+        self.EEG = EEG  # (n,1,258)
+        # (n,1) # Left_Right # Angle /Amplitude (n,2) # position (n,2)
+        self.label = label
 
-        self.EEG = data['EEG']  # (n,1,258)  
-        self.label=data['label'] # (n,2) # Left_Right # Angle /Amplitude (n,3) # position (n,3)
-        
-        assert len(self.EEG) == len(self.label) # Making sure that we have the same no. of mfcc and transcripts    
-       
+        # Making sure that we have the same no. of labels and trials
+        assert len(self.EEG) == len(self.label)
+
         self.length = len(self.EEG)
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, ind):
-        
-        frames = self.EEG[ind] #(1,258)
-        
-        frames = torch.FloatTensor(frames) # Convert to tensors
-        phoneme = torch.tensor(self.label[ind,1:])       
+
+        frames = self.EEG[ind]  # (1,258)
+
+        frames = torch.FloatTensor(frames)  # Convert to tensors
+        phoneme = torch.tensor(self.label[ind, :])
 
         return frames, phoneme
-
-
-# add bench mark here
-
-
-train_data = Dataset(data,partition="train",)
-val_data = Dataset(data,partition="val",)
-test_data = Dataset(data,partition="test",)
-
-
-
-train_loader = torch.utils.data.DataLoader(train_data, num_workers= 4,
-                                           batch_size=config['batch_size'], pin_memory= True,
-                                           shuffle= True)
-
-val_loader = torch.utils.data.DataLoader(val_data, num_workers= 2,
-                                         batch_size=config['batch_size'], pin_memory= True,
-                                         shuffle= False)
-
-test_loader = torch.utils.data.DataLoader(test_data, num_workers= 2, 
-                                          batch_size=config['batch_size'], pin_memory= True, 
-                                          shuffle= False)
