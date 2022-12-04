5b96c3419e634e1f70badcf409ea328cd36d6383
Yidan-Ding <115201635+Yidan-Ding@users.noreply.github.com>
2022-12-04 00:29:32 -0500

    Now the Dataset function is able to work. Created a separate file to split the dataset due to the data size

--- src_single_output/multitask_Dataset.py
+++ src_single_output/multitask_Dataset.py
@@ -41,89 +41,15 @@ def save_label(data_path, variable, IsGenerated, generated_label = None):
     np.savez(data_path,**whole_data)
 
 class Dataset(torch.utils.data.Dataset):
-
-    def __init__(self, data_path, train_ratio, val_ratio, test_ratio, partition):
-        
-        max_ids = 0
-        whole_eeg = []
-        whole_LR_label = []
-        whole_Angle_label = []
-        whole_Amp_label = []
-        whole_Pos_label = []
-        whole_IsGenerated = []
-        whole_ids = []
-        for i in len(data_path):
-            whole_data = np.load(data_path[i], allow_pickle=True)
-            eeg = whole_data['EEG']
-            labels = whole_data['labels']
-            
-            eeg = eeg.transpose((0,2,1))
-            
-            whole_eeg.append(eeg)
-            whole_LR_label.append(whole_data['LR'].item()['label'])
-            whole_Angle_label.append(whole_data['Angle'].item()['label'])
-            whole_Amp_label.append(whole_data['Amplitude'].item()['label'])
-            whole_Pos_label.append(whole_data['Position'].item()['label'])
-
-            IsGenerated = np.array([whole_data['LR'].item()['IsGenerated'], whole_data['Angle'].item()['IsGenerated'],
-            whole_data['Amplitude'].item()['IsGenerated'], whole_data['Position'].item()['IsGenerated']])
-            whole_IsGenerated.append(IsGenerated)
-
-            ids = labels[:, 0]+max_ids
-            max_ids = max(ids)
-            whole_ids.append(ids)
-
-        whole_eeg = np.concatenate(whole_eeg, axis=0)
-        whole_LR_label = np.concatenate(whole_LR_label, axis=0)
-        whole_Angle_label = np.concatenate(whole_Angle_label, axis=0)
-        whole_Amp_label = np.concatenate(whole_Amp_label, axis=0)
-        whole_Pos_label = np.concatenate(whole_Pos_label, axis=0)
-        whole_IsGenerated = np.concatenate(whole_IsGenerated, axis=0)
-        whole_ids = np.concatenate(whole_ids, axis=0)
-
-        # label_min = np.min(whole_label)
-        # label_max = np.max(whole_label)
-
-        # whole_label = (whole_label - label_min)/(label_max - label_min)
-        # self.label_min = label_min
-        # self.label_max = label_max
-
-        # Split the data
-        train_idx, val_idx, test_idx = split(
-            whole_ids, train_ratio, val_ratio, test_ratio)
-
-        if (partition == "train"):
-            EEG = whole_eeg[train_idx]
-            LR_label = whole_LR_label[train_idx]
-            Angle_label = whole_Angle_label[train_idx]
-            Amp_label = whole_Amp_label[train_idx]
-            Pos_label = whole_Pos_label[train_idx]
-            IsGenerated = whole_IsGenerated[train_idx]
-
-        elif (partition == "val"):
-            EEG = whole_eeg[val_idx]
-            LR_label = whole_LR_label[val_idx]
-            Angle_label = whole_Angle_label[val_idx]
-            Amp_label = whole_Amp_label[val_idx]
-            Pos_label = whole_Pos_label[val_idx]
-            IsGenerated = whole_IsGenerated[val_idx]
-        else:
-            EEG = whole_eeg[test_idx]
-            LR_label = whole_LR_label[test_idx]
-            Angle_label = whole_Angle_label[test_idx]
-            Amp_label = whole_Amp_label[test_idx]
-            Pos_label = whole_Pos_label[test_idx]
-            IsGenerated = whole_IsGenerated[test_idx]
-
-        self.EEG = EEG  # (n,1,258)
-        # (n,1) # Left_Right # Angle /Amplitude (n,2) # position (n,2)
-        self.LR_label = LR_label
-        self.Angle_label = Angle_label
-        self.Amp_label = Amp_label
-        self.Pos_label = Pos_label
-        self.IsGenerated = IsGenerated
-
-        # Making sure that we have the same no. of labels and trials
+    def __init__(self, data_path):
+        whole_data = np.load(data_path, allow_pickle=True)
+        self.EEG = whole_data["EEG"]
+        self.LR_label = whole_data["LR_label"]
+        self.Angle_label = whole_data["Angle_label"]
+        self.Amp_label = whole_data["Amp_label"]
+        self.Pos_label = whole_data["Pos_label"]
+        self.IsGenerated = whole_data["IsGenerated"]
+    
         assert len(self.EEG) == len(self.IsGenerated)
 
         self.length = len(self.EEG)
@@ -143,9 +69,7 @@ class Dataset(torch.utils.data.Dataset):
         IsGenerated = torch.tensor(self.IsGenerated[ind])
 
         return raw_eeg, LR_label, Angle_label, Amp_label, Pos_label, IsGenerated
-        # raw_egg [500*128]
-        # lr, angle, amplitue, position(1,2)
-        # IsGenerated [1,4]
+
 
 class TestDataset(torch.utils.data.Dataset): # for generating labels
 
--- src_single_output/multitask_main.py
+++ src_single_output/multitask_main.py
@@ -16,148 +16,151 @@ from models.NewCNN import NewCNN
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
-def main():
 
-    config = {
-        'epochs': 50,
-        'batch_size' : 64,
-        'learning_rate' : 0.0001,
-        'architecture' : 'CNN', # change the model here
-        'task' : 'Direction_task', # 'LR_task'/'Direction_task'/'Position_task' change it here
-        'variable' : 'Amplitude', # 'LR_task': 'LR'; 'Direction_task': 'Angle'/'Amplitude'; 'Position_task': 'Position'
-        'synchronisation' : 'dots_synchronised', #'dots_synchronised',#'processing_speed_synchronised',
-        'hilbert' : False, # with (True) or without (False) hilbert transform
-        'preprocessing' : 'min', # min/max
-        'train_ratio' : 0.7,
-        'val_ratio' : 0.15,
-        'test_ratio' : 0.15
-        
-    }
-
-    # TODO: import as list for datapath
-    data_path = ['./data/LR_task_with_antisaccade_synchronised_min.npz',
-        './data/Direction_task_with_dots_synchronised_min.npz',
-        './data/Position_task_with_dots_synchronised_min.npz'
-        ]
-
-    train_data = Dataset(data_path, train_ratio = config['train_ratio'], val_ratio = config['val_ratio'], test_ratio = config['test_ratio'], partition = 'train')
-    val_data = Dataset(data_path, train_ratio = config['train_ratio'], val_ratio = config['val_ratio'], test_ratio = config['test_ratio'], partition = 'val')
-    test_data = Dataset(data_path, train_ratio = config['train_ratio'], val_ratio = config['val_ratio'], test_ratio = config['test_ratio'], partition = 'test')
-
-
-    train_loader = torch.utils.data.DataLoader(train_data, num_workers=4,
-                                            batch_size=config['batch_size'], pin_memory=True,
-                                            shuffle=True)
-
-    val_loader = torch.utils.data.DataLoader(val_data, num_workers=2,
-                                            batch_size=config['batch_size'], pin_memory=True,
-                                            shuffle=False)
-
-    test_loader = torch.utils.data.DataLoader(test_data, num_workers=2,
-                                            batch_size=config['batch_size'], pin_memory=True,
-                                            shuffle=False)
-
-    input_shape = (129, 500)
-    output_shape = 2 if config['task'] == 'Position_task' else 1 # For position tasks we have two output, but for others only one
+config = {
+    'epochs': 50,
+    'batch_size' : 64,
+    'learning_rate' : 0.0001,
+    'architecture' : 'CNN', # change the model here
+    # 'task' : 'Direction_task', # 'LR_task'/'Direction_task'/'Position_task' change it here
+    # 'variable' : 'Amplitude', # 'LR_task': 'LR'; 'Direction_task': 'Angle'/'Amplitude'; 'Position_task': 'Position'
+    # 'synchronisation' : 'dots_synchronised', #'dots_synchronised',#'processing_speed_synchronised',
+    # 'hilbert' : False, # with (True) or without (False) hilbert transform
+    # 'preprocessing' : 'min', # min/max
+    # 'train_ratio' : 0.7,
+    # 'val_ratio' : 0.15,
+    # 'test_ratio' : 0.15
     
-    if config['architecture'] == 'Xception':
-        model = Xception(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
+}
 
-    elif config['architecture'] == 'CNN':
-        model = CNN(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
+# TODO: import as list for datapath
+train_datapath = "./data/Generated_train.npz"
+val_datapath = "./data/Generated_val.npz"
+test_datapath = "./data/Generated_test.npz"
 
-    elif config['architecture'] == 'PyramidalCNN':
-        model = PyramidalCNN(input_shape, output_shape, kernel_size=16, nb_filters=64, depth=6, batch_size=config['batch_size'])
-
-    elif config['architecture'] == 'NewCNN':
-        model = CNN(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
-      
-
-    # frames,phoneme = next(iter(train_loader))
-
-    model = model.to(device)
-    summary(model,input_shape)
-
-    # criterion = nn.BCEWithLogitsLoss() if config['task']=='LR_task' else nn.MSELoss()
-    # if config['variable'] == 'Angle' and config['task']=='Direction_task':
-    #     criterion = angle_loss
-
-    # Losses
-    lr_criterion = nn.BCEWithLogitsLoss()
-    angle_criterion = angle_loss
-    amplitude_criterion = nn.MSELoss()
-    abs_pos_coriterion = nn.MSELoss()
-    criterion = [lr_criterion, angle_criterion, amplitude_criterion, abs_pos_coriterion]
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate']) #Defining Optimizer
-    # TODO: may change the scheduler later
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, min_lr=0.0001, verbose=True)
-    if config['task']=='LR_task':
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, min_lr=0.0001, verbose=True)
-    scaler = torch.cuda.amp.GradScaler()
-
-    # TODO: may add wandb part later once after there is no bug in the code
-    torch.cuda.empty_cache()
-
-    epochs = config['epochs']
-    # TODO: change how to store the accuracies, store it in separate variables
-    best_acc = 0.0 # Monitor best accuracy in your run
-
-    # Initializing for early stopping 
-    best_val_meansure = 0.0
-    patience_count = 0 
-    patience_max = 20 # TODO: initialized based on paper
-    for epoch in range(config['epochs']):
-        print("\nEpoch {}/{}".format(epoch+1, epochs))
-
-        train_loss, train_pred, train_true = train(model, optimizer, criterion, scaler, train_loader)
-        print("\tTrain Loss: {:.4f}".format(train_loss))
-        print("\tTrain:")
-        train_measure, train_pred = get_output(train_pred, train_true, config['task'],config['variable'])
-        val_pred, val_true = eval(model, val_loader)
-        print("\tValidation:")
-        val_measure, val_pred = get_output(val_pred, val_true, config['task'],config['variable'])
-        
-        ## Early Stopping condition
-        if abs(val_measure - best_val_meansure) > 0.1:
-            best_val_meansure = val_measure
-        else: 
-            patience_count += 1
-
-        if patience_count  >= patience_max:
-            print("\nValid Accuracy didn't improve since last {} epochs.", patience_count)
-            break 
+train_data = Dataset(train_datapath)
+val_data = Dataset(train_datapath)
+test_data = Dataset(train_datapath)
 
 
+train_loader = torch.utils.data.DataLoader(train_data, num_workers=4,
+                                        batch_size=config['batch_size'], pin_memory=True,
+                                        shuffle=True)
 
-        ### Log metrics at each epoch in your run - Optionally, you can log at each batch inside train/eval functions (explore wandb documentation/wandb recitation)
-        # wandb.log({"train loss": train_loss, "validation accuracy": accuracy})
+val_loader = torch.utils.data.DataLoader(val_data, num_workers=2,
+                                        batch_size=config['batch_size'], pin_memory=True,
+                                        shuffle=False)
 
-        ### Save checkpoint if accuracy is better than your current best
-        
-        torch.save({'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': train_loss,
-            'acc': val_measure}, 
-        '../checkpoints/'+config['architecture']+'_'+config['task']+'_checkpoint.pth')
+test_loader = torch.utils.data.DataLoader(test_data, num_workers=2,
+                                        batch_size=config['batch_size'], pin_memory=True,
+                                        shuffle=False)
 
-        
-        scheduler.step(val_measure)
-    #     ## Save checkpoint in wandb
-        #    wandb.save('checkpoint.pth')
+raw_eeg, LR_label, Angle_label, Amp_label, Pos_label, IsGenerated = next(iter(train_loader))
+print(raw_eeg.shape)
+print(LR_label)
+print(Angle_label)
+print(Amp_label)
+print(Pos_label)
+print(IsGenerated)
 
-    #     Is your training time very high? Look into mixed precision training if your GPU (Tesla T4, V100, etc) can make use of it 
-    #     Refer - https://pytorch.org/docs/stable/notes/amp_examples.html
+input_shape = (129, 500)
+output_shape = 2 if config['task'] == 'Position_task' else 1 # For position tasks we have two output, but for others only one
 
-    # ## Finish your wandb run
-    # run.finish()
+if config['architecture'] == 'Xception':
+    model = Xception(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
 
-    test_pred, test_true = test(model, test_loader)
-    print("\tTest:")
-    test_measure, test_pred = get_output(test_pred, test_true, config['task'],config['variable'])
-    results_name = '../results/'+config['architecture']+'_'+config['task']+'_'+config['variable']+".npz"
-    print(results_name)
-    np.savez(results_name, pred = test_pred, truth = test_true, measure = test_measure)
+elif config['architecture'] == 'CNN':
+    model = CNN(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
 
-if __name__=='__multitask_main__':
-    main()
+elif config['architecture'] == 'PyramidalCNN':
+    model = PyramidalCNN(input_shape, output_shape, kernel_size=16, nb_filters=64, depth=6, batch_size=config['batch_size'])
+
+elif config['architecture'] == 'NewCNN':
+    model = CNN(input_shape, output_shape, kernel_size=40, nb_filters=64, depth=6, batch_size=config['batch_size'])
+    
+
+
+model = model.to(device)
+summary(model,input_shape)
+
+# criterion = nn.BCEWithLogitsLoss() if config['task']=='LR_task' else nn.MSELoss()
+# if config['variable'] == 'Angle' and config['task']=='Direction_task':
+#     criterion = angle_loss
+
+# Losses
+lr_criterion = nn.BCEWithLogitsLoss()
+angle_criterion = angle_loss
+amplitude_criterion = nn.MSELoss()
+abs_pos_coriterion = nn.MSELoss()
+criterion = [lr_criterion, angle_criterion, amplitude_criterion, abs_pos_coriterion]
+optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate']) #Defining Optimizer
+# TODO: may change the scheduler later
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, min_lr=0.0001, verbose=True)
+if config['task']=='LR_task':
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, min_lr=0.0001, verbose=True)
+scaler = torch.cuda.amp.GradScaler()
+
+# TODO: may add wandb part later once after there is no bug in the code
+torch.cuda.empty_cache()
+
+epochs = config['epochs']
+# TODO: change how to store the accuracies, store it in separate variables
+best_acc = 0.0 # Monitor best accuracy in your run
+
+# Initializing for early stopping 
+best_val_meansure = 0.0
+patience_count = 0 
+patience_max = 20 # TODO: initialized based on paper
+for epoch in range(config['epochs']):
+    print("\nEpoch {}/{}".format(epoch+1, epochs))
+
+    train_loss, train_pred, train_true = train(model, optimizer, criterion, scaler, train_loader)
+    print("\tTrain Loss: {:.4f}".format(train_loss))
+    print("\tTrain:")
+    train_measure, train_pred = get_output(train_pred, train_true, config['task'],config['variable'])
+    val_pred, val_true = eval(model, val_loader)
+    print("\tValidation:")
+    val_measure, val_pred = get_output(val_pred, val_true, config['task'],config['variable'])
+    
+    ## Early Stopping condition
+    if abs(val_measure - best_val_meansure) > 0.1:
+        best_val_meansure = val_measure
+    else: 
+        patience_count += 1
+
+    if patience_count  >= patience_max:
+        print("\nValid Accuracy didn't improve since last {} epochs.", patience_count)
+        break 
+
+
+
+    ### Log metrics at each epoch in your run - Optionally, you can log at each batch inside train/eval functions (explore wandb documentation/wandb recitation)
+    # wandb.log({"train loss": train_loss, "validation accuracy": accuracy})
+
+    ### Save checkpoint if accuracy is better than your current best
+    
+    torch.save({'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': train_loss,
+        'acc': val_measure}, 
+    '../checkpoints/'+config['architecture']+'_'+config['task']+'_checkpoint.pth')
+
+    
+    scheduler.step(val_measure)
+#     ## Save checkpoint in wandb
+    #    wandb.save('checkpoint.pth')
+
+#     Is your training time very high? Look into mixed precision training if your GPU (Tesla T4, V100, etc) can make use of it 
+#     Refer - https://pytorch.org/docs/stable/notes/amp_examples.html
+
+# ## Finish your wandb run
+# run.finish()
+
+test_pred, test_true = test(model, test_loader)
+print("\tTest:")
+test_measure, test_pred = get_output(test_pred, test_true, config['task'],config['variable'])
+results_name = '../results/'+config['architecture']+'_'+config['task']+'_'+config['variable']+".npz"
+print(results_name)
+np.savez(results_name, pred = test_pred, truth = test_true, measure = test_measure)
+
